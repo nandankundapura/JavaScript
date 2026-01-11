c471460a5038c77e1e4274116e9418f774e211aa
nandankundapura <nandankundapura777@gmail.com>
2026-01-11 13:33:43 +0530

    JS- App

---
+++ app.js
@@ -0,0 +1,33 @@
+// alert("hello");
+console.log(123);
+console.log("Nandan");
+console.log(true);
+var greeting="hello";
+console.log(greeting);
+
+console.log([1,2,3,4]);
+
+console.log({a:1, b:2, c:3});
+
+var name="Nandan";
+ //console.log(name);
+ name="Nand";
+ console.log(name);
+
+ var greeting;
+ greeting= "Hi";
+ console.log(greeting);
+
+ var $name="John";
+ console.log(name);
+
+ var firstName="Nandan";  // camelcase
+ var first_name="Nandan"; // underscore
+ var firstName="Nandan"; 
+
+ let Name="Nandana";
+ console.log(name);
+
+ const numbers=[1,2,3,4];
+// numbers.push(6);
+ console.log(numbers);
