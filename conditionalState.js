b3e30542b52ee84804f3a4ccf6e04b791d8d3dc9
nandankundapura <nandankundapura777@gmail.com>
2026-01-11 13:36:31 +0530

    JS- Conditional Statements

---
+++ conditionalState.js
@@ -0,0 +1,31 @@
+//if statement
+let age=20;
+if(age>=18);
+{
+    console.log("Welcome to the club");
+}
+
+//if else
+let isVip=true;
+if(isVip==true)
+{
+    console.log("Welcome to the vip lounge");
+}
+else{
+    console.log("Welcom to the regular lounge");
+}
+
+//if ,  else if,  else
+let Age=70;
+if(Age<12)
+{
+    console.log("Child ticket");
+}
+else if(Age>=12)
+{
+    console.log("adult price");
+}
+else
+{
+    console.log("senior citizen price");
+}
