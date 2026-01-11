534e7170198c2c1e95eb3f68b1ced4dd46edeb93
nandankundapura <nandankundapura777@gmail.com>
2026-01-11 13:30:53 +0530

    JS- Datatypes

---
+++ datatype.js
@@ -0,0 +1,40 @@
+//Primitive  datatype
+//String
+const name="Bharath";
+console.log(typeof name);  
+
+//number
+const age=10;
+console.log(typeof age);
+
+const num=10;
+console.log(typeof num);
+
+//boolean
+const hasSiblings=true;
+console.log(typeof hasSiblings);
+ 
+//null
+const abc=null;  // we are storing a value of nothing
+console.log(typeof abc);
+
+//undefined
+let a; // if we don't initilize it with a value, then it is undefined, by defalut variable are undefined
+
+const sym=Symbol();
+console.log(typeof sym);
+
+
+
+//Reference datatype
+//Array
+const hobbies=["movie, cricket"];
+console.log(typeof hobbies);
+
+//object literals
+const person={
+    name:"nandan",
+    age:25,
+}
+console.log(typeof person);
+
