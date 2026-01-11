ef1ec1b90063089f4765897d1dc6d231bcb0d3f1
nandankundapura <nandankundapura777@gmail.com>
2026-01-11 13:37:12 +0530

    JS- Example

---
+++ exmp.js
@@ -0,0 +1,48 @@
+
+let a=23;
+console.log(a);
+console.log(typeof a);
+
+let b=1/0;
+console.log(typeof b);
+
+let c=-1/0;
+console.log(typeof c);
+
+let d="hello"/2;
+console.log(typeof d);
+
+
+//range of variable -ve
+ let e=-9007199254740991;
+console.log(typeof e);
+
+//range of variable +ve
+ let f=9007199254740991;
+console.log(typeof f);
+
+ let g=900719925474099100n;
+console.log(typeof g);
+
+ let h=100n;
+console.log(typeof h);
+
+//add numbers
+let j=100n;
+let k=200n;
+console.log(j+k);
+
+//string
+let n="nandan"
+console.log(n);
+
+//boolean
+let hasPassport=false;
+console.log(hasPassport);
+
+//undefined
+let o;
+console.log(o);
+
+
+
