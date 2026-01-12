f1b6ec383dfdd0101da1bbb941c2f0820d632ab0
nandankundapura <nandankundapura777@gmail.com>
2026-01-12 13:08:20 +0530

    JS- Loops

---
+++ loop.js
@@ -0,0 +1,22 @@
+let i;
+for(i=0;i<=2;i++)
+{
+    console.log("*");
+}
+
+//while loop
+let j;
+j=1;
+while(j<=2)
+{
+    console.log("*");
+    j++;
+}
+
+//do while -> Atleast once execution always happen
+k=1;
+do
+{
+    console.log("*");
+    k++;
+}while(k<=2);
