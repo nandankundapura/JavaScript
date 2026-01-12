330c4b52ee14d7d22b64acba2d7d212d6a586287
nandankundapura <nandankundapura777@gmail.com>
2026-01-12 13:10:29 +0530

    JS- Ternary Operator

---
+++ ternaryOper.js
@@ -0,0 +1,21 @@
+let age=18;
+let message;
+if(age>=18)
+{
+    message="can vote";
+}
+else
+{
+    message="cannot vote";
+}
+console.log(message);
+
+//using ternary operator
+//syntax :  condition ? expression1 : expression2
+
+let Age=16;
+let Message;
+
+Message=Age>=18 ? "can vote" : "cannot vote";
+console.log(Message);
+
