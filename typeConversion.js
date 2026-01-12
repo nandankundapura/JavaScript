8f46fbe3e212d49656f8f98e56da4e5e69baad3a
nandankundapura <nandankundapura777@gmail.com>
2026-01-12 13:11:48 +0530

    JS- Type Conversion

---
+++ typeConversion.js
@@ -0,0 +1,44 @@
+let val;
+
+//Number to a String
+val=5;
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+
+//to string
+val='5';
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+
+//string to number
+val=5;
+console.log(val);
+console.log(typeof val);
+console.log(val.toFixed(2));
+
+
+//boolean value  true
+val=Number(true);
+console.log(val);
+console.log(typeof val);
+console.log(val.toFixed(2));
+
+//boolean value  false
+val=Number(false);
+console.log(val);
+console.log(typeof val);
+console.log(val.toFixed(2));
+
+// to null
+val=Number(null);
+console.log(val);
+console.log(typeof val);
+console.log(val.toFixed(2));
+
+// to null
+val=parseInt("5");
+console.log(val);
+console.log(typeof val);
+console.log(val.toFixed(2));
