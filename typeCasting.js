cae09074dfe0eab28bf6791851212b6016ebbfa0
nandankundapura <nandankundapura777@gmail.com>
2026-01-12 13:11:00 +0530

    JS- Type Casting

---
+++ typeCasting.js
@@ -0,0 +1,122 @@
+//number to string
+let a=123;
+console.log(a);
+console.log(typeof a);
+let b=String(a);
+console.log(b);
+console.log(typeof b);
+
+//boolean to string
+let c=false;
+console.log(c);
+console.log(typeof c);
+let d=String(c);
+console.log(d);
+console.log(typeof d);
+
+//string to number
+let e="123";
+console.log(e);
+console.log(typeof e);
+let f=Number(e);
+console.log(f);
+console.log(typeof f);
+
+let g="123Nandan";
+console.log(g);
+console.log(typeof g);
+let h=Number(g);
+console.log(g);
+console.log(typeof h);
+
+// use of parseInt
+let i="123nandan";
+console.log(i);
+console.log(typeof i);
+let j=parseInt(i);
+console.log(j);
+console.log(typeof j);
+
+let k="nandan123";
+console.log(k);
+console.log(typeof k);
+let l=parseInt(k);
+console.log(l);
+console.log(typeof l);
+
+let m=" 123Nandan";
+console.log(m);
+console.log(typeof m);
+let n=parseInt(m);
+console.log(n);
+console.log(typeof n);
+
+let p=" 1 2 3";
+console.log(p);
+console.log(typeof p);
+let q=parseInt(p);
+console.log(q);
+console.log(typeof q);
+
+//boolean to number
+let r=true;
+console.log(r);
+console.log(typeof r);
+let s=Number(r);
+console.log(s);
+console.log(typeof s);
+
+let t=false;
+console.log(t);
+console.log(typeof t);
+let u=Number(t);
+console.log(u);
+console.log(typeof u);
+
+//number to boolean
+let v=123;
+console.log(v);
+console.log(typeof v);
+console.log(Boolean(v));
+
+let w=-123;
+console.log(w);
+console.log(typeof w);
+console.log(Boolean(w));
+
+let x=Infinity;
+console.log(x);
+console.log(typeof x);
+console.log(Boolean(x));
+
+let y=-Infinity;
+console.log(y);
+console.log(typeof y);
+console.log(Boolean(y));
+
+let z="hello"/2;  //NaN
+console.log(z);
+console.log(typeof z);
+console.log(Boolean(z));
+
+//string to boolean
+let aa="hello";  
+console.log(aa);
+console.log(typeof aa);
+console.log(Boolean(aa));
+
+let bb="123";  
+console.log(bb);
+console.log(typeof bb);
+console.log(Boolean(bb));
+
+let cc=" ";  
+console.log(cc);
+console.log(typeof cc);
+console.log(Boolean(cc));
+
+let dd="";  
+console.log(dd);
+console.log(typeof dd);
+console.log(Boolean(dd));
+
