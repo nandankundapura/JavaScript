9187b14b3105adad88abbfa32949c49885203ac6
nandankundapura <nandankundapura777@gmail.com>
2026-01-15 22:03:34 +0530

    JS- Call Back Functions

---
+++ callBackFunctions1.js
@@ -0,0 +1,19 @@
+function add(a,b)
+{
+    return a+b;
+}
+function sub(a,b)
+{
+    return a-b;
+}
+function mul(a,b)
+{
+    return a*b;
+}
+function calc(a,b,callback)
+{
+    return callback(a,b);
+}
+console.log(calc(10,20,add));
+console.log(calc(10,20,sub));
+console.log(calc(10,20,mul));
