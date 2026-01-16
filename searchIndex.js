eacc9f935a8a1f0d45bb35b77554357f739fd30c
nandankundapura <nandankundapura777@gmail.com>
2026-01-16 22:33:17 +0530

    JS- Search Index

---
+++ searchIndex.js
@@ -0,0 +1,8 @@
+let ar=[10,20,30,40,50,30];
+console.log(ar);
+console.log(ar.indexOf(30));
+console.log(ar.indexOf(500));  //not insidein array give -1 as output
+console.log(ar.lastIndexOf(30));
+console.log(ar.includes(40));
+console.log(ar.includes(600));
+
