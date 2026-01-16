b678d860db9a90ee7897793007ecde7c36a02d7c
nandankundapura <nandankundapura777@gmail.com>
2026-01-16 22:31:29 +0530

    JS- Array Operations

---
+++ arrayOperations.js
@@ -0,0 +1,14 @@
+let arr=[10,20,30,40,50];
+console.log(arr);
+arr.push(60);       //adding at ending
+console.log(arr);
+arr.unshift(70);    //adding at beginning
+console.log(arr);
+arr.pop();          //remove at last
+console.log(arr);
+
+let x=arr.pop();
+console.log(x);
+let y=arr.shift();  //removingfirst element
+console.log(y);
+console.log(arr);
