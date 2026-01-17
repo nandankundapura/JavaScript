826ebce08312c0bed894511a04ad5a960797f031
nandankundapura <nandankundapura777@gmail.com>
2026-01-17 20:14:57 +0530

    JS - Find Method

---
+++ find.js
@@ -0,0 +1,11 @@
+let score=[85,72,90,67];
+let res=score.find((x)=>x>80);
+console.log(res);
+
+
+//exp2
+let score1=[85,72,90,67];
+let res1=score1.find((y)=>y>85);
+console.log(res1);
+let res2=score.findIndex((z)=>z>85);
+console.log(res2);
