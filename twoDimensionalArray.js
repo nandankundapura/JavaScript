94b3d31549fc164ecbba941cd82c57c21b6ff3ec
nandankundapura <nandankundapura777@gmail.com>
2026-01-21 10:08:37 +0530

    JS - Two Dimensional Array

---
+++ twoDimensionalArray.js
@@ -0,0 +1,15 @@
+let ar=[
+    [25,30,22],
+    [23,31,24],
+    [26,30,20]
+];
+console.log(ar);
+let i;
+let j;
+for(i=0;i<ar.length;i++)
+{
+    for(j=0;j<ar[i].length;j++)
+    {
+        console.log(ar[i][j]);
+    }
+}
