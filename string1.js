eb19a3b17562528f33d2c7e72128250de1cb777b
nandankundapura <nandankundapura777@gmail.com>
2026-01-21 10:07:11 +0530

    JS - Strings

---
+++ string1.js
@@ -0,0 +1,16 @@
+//creating string in 3 ways
+let a1="JavaScript";        //using double quote
+let name='Rohit';           //using single quote
+let greeting=`Hi,${name}`; //using backtrick  ``
+console.log(a1);
+console.log(name);
+console.log(greeting);
+
+//String comparison
+let s1="JavaScript";
+let s2="Java";
+console.log(s1==s2);
+let s3="java";
+console.log(s2==s3);
+
+//Strings are case sensitive
